cfb038475d4453407d330d38285ba35a43fa2c25
Arunmozhi <arunmozhi@opencraft.com>
2025-04-14 23:05:12 +1000

    refactor: pull up scrolling to Iframe method as a callback prop

--- src/components/UnitPageSidebar.tsx
+++ src/components/UnitPageSidebar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {AspectsSidebar} from './AspectsSidebar';
 import {Block} from "../hooks";
 import {BLOCK_TYPES} from "../constants";
+import {useIframe} from 'CourseAuthoring/generic/hooks/context/hooks';
 
 interface Props {
   courseId: string;
@@ -13,14 +14,15 @@ interface Props {
 export const UnitPageSidebar = ({courseId, blockId, unitTitle, verticalBlocks}: Props) => {
   const problemBlocks = React.useMemo(() => verticalBlocks.filter(block => block.blockType === "problem"), [verticalBlocks]);
   const videoBlocks = React.useMemo(() => verticalBlocks.filter(block => block.blockType === "video"), [verticalBlocks]);
+  const {sendMessageToIframe} = useIframe();
 
   return <AspectsSidebar
     title={unitTitle}
     blockType={BLOCK_TYPES.vertical}
     hasDashboard={false}
     dashboardId={blockId}
-    subsections={null}
     problemBlocks={problemBlocks}
     videoBlocks={videoBlocks}
+    blockActivatedCallback={(block: Block) => sendMessageToIframe("scrollToXBlock", {locator: block.id})}
   />
 }
